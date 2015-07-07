45d0967f48ed9cd215bcc9f30711abdf6de6349a
caichuanze <caichuanze@icloud.com>
2015-07-07 20:17:25 +0800

    day1

---
+++ Scanner.java
@@ -0,0 +1,36 @@
+package Day1;
+
+import java.io.InputStream;
+
+public class Scanner {
+
+	public Scanner(InputStream in) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scanner=new Scanner(System.in);
+boolean bool=scanner.hasNext();
+System.out.print(bool);
+	}
+
+	private boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public String nextLine() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public float nextFloat() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ scannerchas.java
@@ -0,0 +1,23 @@
+package Day1;
+
+public class scannerchas {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scanner=new Scanner(System.in);
+		    String ch1=scanner.nextLine();	
+		    String ch2=scanner.nextLine();
+		    String ch3=scanner.nextLine();
+		    String ch4=scanner.nextLine();
+		    float ch5=scanner.nextFloat();
+		    float ch6=scanner.nextFloat();	
+		    String ch7=ch1+ch2;
+		    System.out.print(ch7);
+		    System.out.print(ch3*ch4);
+		    System.out.print(ch5/ch6);
+	}
+
+}
